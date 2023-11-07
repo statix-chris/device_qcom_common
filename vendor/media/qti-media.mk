205225a6e6968149b554ab8759630e1fa3d25504
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-11-07 12:33:47 +0000

    common: Enable TARGET_HALS_VARIANT and adapt makefiles inherit to AOSP
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/media/qti-media.mk
+++ vendor/media/qti-media.mk
@@ -18,7 +18,7 @@ PRODUCT_SOONG_NAMESPACES += \
 TARGET_MEDIA_COMPONENT_VARIANT := media
 
 # Inherit configuration from the HAL.
-$(call inherit-product-if-exists, hardware/qcom/media/product.mk)
+$(call inherit-product-if-exists, $(TARGET_HALS_PATH)/media/product.mk)
 
 # Enable 64-bit mediaserver
 PRODUCT_VENDOR_PROPERTIES += \
